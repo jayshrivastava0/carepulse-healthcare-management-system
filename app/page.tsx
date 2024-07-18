55a7f8fab11448c5b5131cde6623c0c24c245034
jayshrivastava0 <sujay1829@gmail.com>
2024-07-18 17:59:45 -0400

    update homepage

--- app/page.tsx
+++ app/page.tsx
@@ -10,28 +10,28 @@ export default function Home() {
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image
-          src = "/assets/icons/logo-full.svg"
-          height = {1000}
-          width = {1000}
-          alt = "patient"
-          className="mb-12 h-10 w-fit"
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="patient"
+            className="mb-12 h-10 w-fit"
           />
-          <PatientForm/>
+          <PatientForm />
           <div className="text-14-regular mt-20 flex justify-between">
             <p className="justify-items-end text-dark-600 xl:text-left">
-            © July 2024 CarePulse. All fake rights reserved by Jay Shrivastava.
+              © July 2024 CarePulse. All fake rights reserved by Jay Shrivastava.
             </p>
             <Link href="/?admin=true" className="text-green-500">
-              "Admin"
+              Admin
             </Link>
           </div>
         </div>
       </section>
       <Image
-        src = "/assets/images/onboarding-img.png"
-        height = {1000}
-        width = {1000}
-        alt = "patient"
+        src="/assets/images/onboarding-img.png"
+        height={1000}
+        width={1000}
+        alt="patient"
         className="side-img max-w-[50%]"
       />
     </div>
