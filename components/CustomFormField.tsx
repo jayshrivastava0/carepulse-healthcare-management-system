ede6120f7b69d805e003f1e61954684bfc71747a
jayshrivastava0 <sujay1829@gmail.com>
2024-07-18 21:49:37 -0400

    Home-page-login Frontend

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -1,5 +1,6 @@
 'use client'
 import React from "react";
+import NextImage from 'next/image';
 
 import {
     FormControl,
@@ -10,29 +11,87 @@ import {
     FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Control } from "react-hook-form";
+import { Control, Form } from "react-hook-form";
+import { FormFieldType } from "@/components/forms/PatientForms";
+import { Image } from "lucide-react";
+import 'react-phone-number-input/style.css'
+import PhoneInput from 'react-phone-number-input'
+import { E164Number } from 'libphonenumber-js/core'
+
 
 interface CustomProps {
-    control: Control<any>;
+    control: Control<any>,
+    fieldType: FormFieldType,
+    name: string,
+    label?: string,
+    placeholder?: string,
+    iconSrc?: string,
+    iconAlt?: string,
+    disabled?: boolean,
+    dateFormat?: string,
+    showTimeSelect?: boolean,
+    children?: React.ReactNode,
+    renderSkeleton?: (field: any) => React.ReactNode,
 }
 
+const RenderField = ({ field, props }: { field: any, props: CustomProps }) => {
+    const { fieldType, iconSrc, iconAlt, placeholder } = props;
 
+    switch (props.fieldType) {
+        case FormFieldType.INPUT:
+            return (
+                <div className="flex rounded-md border border-dark-500 bg-dark-400">
+                    {iconSrc && (
+                        <NextImage
+                            src={iconSrc}
+                            height={24}
+                            width={24}
+                            alt={iconAlt || 'icon'}
+                            className="ml-2"
+                        />
+                    )}
+                    <FormControl>
+                        <Input 
+                            placeholder={placeholder}
+                            {...field}
+                            className="shad-input border-0"
+                        />
+                    </FormControl>
+                </div>
+            )
+        case FormFieldType.PHONE_INPUT:
+            return (
+                <FormControl>
+                    <PhoneInput 
+                        defaultCountry="US"
+                        placeholder={placeholder}
+                        international
+                        withCountryCallingCode
+                        value={field.value as E164Number | undefined}
+                        onChange={field.onChange}
+                        className=" input-phone shad-input border-0"
+                    />
+                </FormControl>
+            )
 
-const CustomFormField = ({ control }: CustomProps) => {
+        default:
+            break;
+        }
+}
+
+const CustomFormField = (props: CustomProps) => {
+    const { control, fieldType, name, label } = props;
     return (
         <FormField
             control={control}
-            name="username"
+            name={name}
             render={({ field }) => (
-                <FormItem>
-                    <FormLabel>Username</FormLabel>
-                    <FormControl>
-                        <Input placeholder="shadcn" {...field} />
-                    </FormControl>
-                    <FormDescription>
-                        This is your public display name.
-                    </FormDescription>
-                    <FormMessage />
+                <FormItem className="flex-1">
+                    {fieldType !== FormFieldType.CHECKBOX && label && (
+                        <FormLabel>{label}</FormLabel>
+                    )}
+                    <RenderField field={field} props={props} />
+                    <FormMessage className="shad-error" />
                 </FormItem>
             )}
         />
--- components/forms/PatientForms.tsx
+++ components/forms/PatientForms.tsx
@@ -6,29 +6,50 @@ import { z } from "zod"
 import { Button } from "@/components/ui/button"
 import { Form } from "@/components/ui/form"
 import CustomFormField from "../CustomFormField"
+import SubmitButton from "../SubmitButton"
+import { useState } from "react"
+import { UserFormValidation } from "@/lib/validation"
+import { useRouter } from "next/navigation"
+
+export enum FormFieldType {
+    INPUT = 'input',
+    TEXTAREA = 'textarea',
+    PHONE_INPUT = 'phoneInput',
+    CHECKBOX = 'checkbox',
+    DATE_PICKER = 'datePicker',
+    SELECT = 'select',
+    SKELETON = 'skeleton',
+}
 
 
-
-const formSchema = z.object({
-    username: z.string().min(2, {
-        message: "Username must be at least 2 characters.",
-    }),
-})
 
 const PatientForm = () => {
+    const router = useRouter();
+    const [isLoading, setIsLoading] = useState(false)
     // 1. Define your form.
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
+    const form = useForm<z.infer<typeof UserFormValidation>>({
+        resolver: zodResolver(UserFormValidation),
         defaultValues: {
-            username: "",
+            name: "",
+            email: "",
+            phone: "",
         },
     })
 
     // 2. Define a submit handler.
-    function onSubmit(values: z.infer<typeof formSchema>) {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
-        console.log(values)
+    async function onSubmit({ name, email, phone }: z.infer<typeof UserFormValidation>) {
+        setIsLoading(true);
+
+        try {
+            // const userData = { name, email, phone};
+            // const user = await createUser(userData);
+            
+            // if(user) router.push(`patients/${user.$id}/register`)
+        } catch (error) {
+            console.log(error)
+        }
+
+
     }
     return (
         <Form {...form}>
@@ -42,10 +63,34 @@ const PatientForm = () => {
                     </p>
                 </section>
                 <CustomFormField
+                    fieldType={FormFieldType.INPUT}
                     control={form.control}
+                    name='name'
+                    label='Full Name'
+                    placeholder='Jay Shrivastava'
+                    iconSrc='/assets/icons/user.svg'
+                    iconAlt='user'
                 />
 
-                <Button type="submit">Submit</Button>
+                <CustomFormField
+                    fieldType={FormFieldType.INPUT}
+                    control={form.control}
+                    name='email'
+                    label='Email'
+                    placeholder='sujay-shrivastava-email@gmail.com'
+                    iconSrc='/assets/icons/email.svg'
+                    iconAlt='user'
+                />
+
+                <CustomFormField
+                    fieldType={FormFieldType.PHONE_INPUT}
+                    control={form.control}
+                    name='phone'
+                    label='Phone Number'
+                    placeholder='(xxx)-xxx-xxxx'
+                />
+
+                <SubmitButton isLoading={isLoading}>Get Started </SubmitButton>
             </form>
         </Form>
     )
